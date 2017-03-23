725f1d1ae6a76ec96790cdec02910d655058170d
zq32 <zq32@cornell.edu>
2017-03-23 12:19:02 -0400

    add probabilistic logic

--- reaction_paper_1.py
+++ reaction_paper_1.py
@@ -43,16 +43,15 @@ def get_node_color(G, attr):
 """
 original iterate function
 A can be changed by a lot of B
-contains bugs
 
 """
-def graph_naive_iterate(G, steps, plot=False):
-    pos = nx.spring_layout(G)
+def graph_naive_iterate(G, steps, plot=False, debug=False):
     for t in range(steps):
         if plot:
             h = int(np.sqrt(steps))
-            plt.subplot(h, ceil(steps / h), t + 1)
-            nx.draw_networkx(G, pos, node_color=get_node_color(G, 'attr'))
+            plt.subplot(h,ceil(steps / h),t + 1)
+            n_size = 200 if debug else 50
+            nx.draw_networkx(G, pos, node_color=get_node_color(G, 'attr'), with_labels = debug, node_size=n_size)
             plt.axis('off')
 #            plt.show()
         
@@ -95,40 +94,106 @@ def graph_naive_iterate(G, steps, plot=False):
                 res[np.argmax(node_attr)] = 1
                 G.node[idx]['attr'] = res
                 G.node[idx]['temp'] = np.array([0,0,0])
+    plt.show()
                 
+def graph_iterate(G, steps, plot=False, debug=False):
+    for t in range(steps):
+        if plot:
+            h = int(np.sqrt(steps))
+            plt.subplot(h,ceil(steps / h),t + 1)
+            n_size = 200 if debug else 50
+            nx.draw_networkx(G, pos, node_color=get_node_color(G, 'attr'), with_labels = debug, node_size=n_size)
+            plt.axis('off')
+            if debug:
+                plt.show()
                 
+        for idx in G.nodes():
+            G.node[idx]['attr'] = G.node[idx]['attr'].astype(np.float32, copy = False)
+            G.node[idx]['temp'] = G.node[idx]['temp'].astype(np.float32, copy = False)
+            
+            
+    # phase 1
+            
+        for idx in G.nodes():
+            node = G.node[idx]
+            node_attr = node['attr']
+            node_color_idx = np.argmax(node_attr)
+            for neibor_idx in G.neighbors(idx):
+                neibor_node = G.node[neibor_idx]
+                neibor_attr = neibor_node['attr']
+                if np.array_equal(np.array([0,0,0], dtype='f'), neibor_attr):
+                    continue
+                else:
+                    neibor_color_idx = np.argmax(neibor_attr)
+                    if neibor_color_idx == 0:
+                        G.node[idx]['temp'][0] += neibor_attr[0] * A
+                        G.node[idx]['temp'][2] += neibor_attr[0] * A
+                    elif neibor_color_idx == 1:
+                        G.node[idx]['temp'][1] += neibor_attr[1] * B
+                        G.node[idx]['temp'][2] += neibor_attr[1] * B
+                    elif neibor_color_idx == 2:
+                        G.node[idx]['temp'][0] += neibor_attr[2] * A
+                        G.node[idx]['temp'][1] += neibor_attr[2] * B
+                        G.node[idx]['temp'][2] += neibor_attr[2] * max(A, B)
+            if G.node[idx]['temp'][2] - 0 > 1e-1:
+                G.node[idx]['temp'][2] += AB
+                
+        
+    # phase 2 
+        for idx in G.nodes():
+            node = G.node[idx]
+            node_attr = node['temp']
+            if np.array_equal(node_attr, np.array([0, 0 ,0], dtype='f')):
+                continue
+            elif np.argmax(G.node[idx]['attr']) == 0 and G.node[idx]['attr'][0] != 0:
+                G.node[idx]['temp'] = np.array([0,0,0], dtype='f')
+            else:
+                res = G.node[idx]['temp']/sum(G.node[idx]['temp'])
+                G.node[idx]['attr'] = res
+                G.node[idx]['temp'] = np.array([0,0,0], dtype='f')
+                
+    plt.show()
+            
         
 # initialize process
 A = 1
 B = 2
-AB = -1
+AB = -.5
 
 
-num_nodes = 6
+num_nodes = 10
+debug = False
+if debug:
+    G = nx.read_gpickle('graph.pk')
+else:
+    while(True):
+        G = nx.gnp_random_graph(num_nodes, 0.3)
+        if nx.is_connected(G):
+            break
+    for node_idx in G.nodes():
+        G.node[node_idx]['attr'] = np.array([0, 0, 0])
+        G.node[node_idx]['temp'] = np.array([0, 0, 0])
+    #nx.set_node_attributes(G, "t", 0)
+    # initialize infections
+    a_init = ceil(0.1 * num_nodes)
+    b_init = ceil(0.1 * num_nodes)
+    
+    steps = 10
+    random_nodes = choice(num_nodes, a_init + b_init, replace=False)
+    a_init_nodes_idx = random_nodes[:a_init]
+    b_init_nodes_idx = random_nodes[a_init:a_init+b_init]
+    
+    for i in a_init_nodes_idx:
+        G.node[i]['attr'] = np.array([1,0,0])
+    for i in b_init_nodes_idx:
+        G.node[i]['attr'] = np.array([0,1,0])
+        
+    nx.write_gpickle(G, 'graph.pk')
 
-while(True):
-    G = nx.gnp_random_graph(num_nodes, 0.3)
-    if nx.is_connected(G):
-        break
-for node_idx in G.nodes():
-    G.node[node_idx]['attr'] = np.array([0, 0, 0])
-    G.node[node_idx]['temp'] = np.array([0, 0, 0])
-#nx.set_node_attributes(G, "t", 0)
-# initialize infections
-a_init = ceil(0.1 * num_nodes)
-b_init = ceil(0.1 * num_nodes)
-
-steps = 10
-random_nodes = choice(num_nodes, a_init + b_init, replace=False)
-a_init_nodes_idx = random_nodes[:a_init]
-b_init_nodes_idx = random_nodes[a_init:a_init+b_init]
-
-for i in a_init_nodes_idx:
-    G.node[i]['attr'] = np.array([1,0,0])
-for i in b_init_nodes_idx:
-    G.node[i]['attr'] = np.array([0,1,0])
-
-graph_naive_iterate(G, steps, True)
+pos = nx.spring_layout(G)
+G2 = G.copy()
+graph_naive_iterate(G, steps, True, debug)
+graph_iterate(G2, steps, True, debug)
 
     
     
